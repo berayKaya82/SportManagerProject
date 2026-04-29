f60b18dfddbbedb669ba35821171da33cf8dc1f4
durueda <durueda@gmail.com>
2026-04-29 22:34:58 +0300

    wrote the scenemanager and the app classes
    i added library and methods for scene and for the window

--- src/main/java/ui/App.java
+++ src/main/java/ui/App.java
@@ -1,4 +1,23 @@
 package ui;
 
-public class App {
+import application.DefaultGameFacade;
+import application.GameFacade;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private final GameFacade facade = new DefaultGameFacade();
+
+    @Override
+    public void start(Stage stage) {
+        SceneManager.getInstance().setStage(stage);
+        SceneManager.getInstance().switchTo("main-menu", facade);
+
+        stage.setTitle("Sports Manager");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
